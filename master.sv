8853af528acabde7307e1ec71462e6777bad08b5
a55446262 <a55446262@gmail.com>
2022-10-16 17:31:20 +0800

    add commmit

--- master.sv
+++ master.sv
@@ -3,16 +3,21 @@
 
 modport Slave(
 
+//WRITE DATA CHANNEL
 	output	WREADY,     //write ready signal from slave
 	input	WVALID,		//valid signal for write 
 	input	WLAST,		//write last signal
 	input	WSTRB,		// strobe signal for writing in
 	input	WDATA,		//write data
 	input	WID,        //write data id
+
+//WRITE RESPONSE CHANNEL
 	output	BID, 		//response id
 	output	BRESP,	 	//write response signal from slave
 	output	BVALID,     //write response valid signal
 	input	BREADY,     //write response ready signal
+
+//WRITE ADDRESS CHANNEL
 	output	AWREADY,    //write address ready signal from slave
 	input	AWVALID,    // write address valid signal
 	input	AWBURST,	//write address channel signal for burst type
@@ -20,6 +25,8 @@ modport Slave(
 	input	AWLEN,      //burst length- number of transfers in a burst
 	input	AWADDR,     //write address signal 
 	input	AWID,		// write address id 
+
+//READ ADDRESS CHANNEL
 	output	ARREADY,  //read address ready signal from slave
 	input	ARID,      //read address id
 	input	ARADDR,		//read address signal
@@ -27,6 +34,8 @@ modport Slave(
 	input	ARSIZE,		//number of bytes in a transfer
 	input	ARBURST,	//burst type - fixed, incremental, wrapping
 	input	ARVALID,	//address read valid signal
+
+//READ DATA CHANNEL
 	output	RID,		//read data id
 	output	RDATA,     //read data from slave
  	output	RRESP,		//read response signal
