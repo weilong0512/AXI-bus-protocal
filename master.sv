bed8f5895922baac37f99d7a88e55fb0fa6216eb
a55446262 <a55446262@gmail.com>
2022-10-16 17:38:05 +0800

    add logic

--- master.sv
+++ master.sv
@@ -2,44 +2,43 @@
 `define SIZE 3
 
 modport Slave(
-
 //WRITE DATA CHANNEL
-	output	WREADY,     //write ready signal from slave
-	input	WVALID,		//valid signal for write 
-	input	WLAST,		//write last signal
-	input	WSTRB,		// strobe signal for writing in
-	input	WDATA,		//write data
-	input	WID,        //write data id
+	output logic WREADY,     //write ready signal from slave
+	input logic WVALID,		//valid signal for write 
+	input logic WLAST,		//write last signal
+	input logic WSTRB,		// strobe signal for writing in
+	input logic WDATA,		//write data
+	input logic WID,        //write data id
 
 //WRITE RESPONSE CHANNEL
-	output	BID, 		//response id
-	output	BRESP,	 	//write response signal from slave
-	output	BVALID,     //write response valid signal
-	input	BREADY,     //write response ready signal
+	output logic BID, 		//response id
+	output logic BRESP,	 	//write response signal from slave
+	output logic BVALID,     //write response valid signal
+	input logic BREADY,     //write response ready signal
 
 //WRITE ADDRESS CHANNEL
-	output	AWREADY,    //write address ready signal from slave
-	input	AWVALID,    // write address valid signal
-	input	AWBURST,	//write address channel signal for burst type
-	input	AWSIZE,     //size of each transfer in bytes(encoded)
-	input	AWLEN,      //burst length- number of transfers in a burst
-	input	AWADDR,     //write address signal 
-	input	AWID,		// write address id 
+	output logic AWREADY,    //write address ready signal from slave
+	input logic AWVALID,    // write address valid signal
+	input logic AWBURST,	//write address channel signal for burst type
+	input logic AWSIZE,     //size of each transfer in bytes(encoded)
+	input logic AWLEN,      //burst length- number of transfers in a burst
+	input logic AWADDR,     //write address signal 
+	input logic AWID,		// write address id 
 
 //READ ADDRESS CHANNEL
-	output	ARREADY,  //read address ready signal from slave
-	input	ARID,      //read address id
-	input	ARADDR,		//read address signal
-	input	ARLEN,      //length of the burst
-	input	ARSIZE,		//number of bytes in a transfer
-	input	ARBURST,	//burst type - fixed, incremental, wrapping
-	input	ARVALID,	//address read valid signal
+	output logic ARREADY,  //read address ready signal from slave
+	input logic ARID,      //read address id
+	input logic ARADDR,		//read address signal
+	input logic ARLEN,      //length of the burst
+	input logic ARSIZE,		//number of bytes in a transfer
+	input logic ARBURST,	//burst type - fixed, incremental, wrapping
+	input logic ARVALID,	//address read valid signal
 
 //READ DATA CHANNEL
-	output	RID,		//read data id
-	output	RDATA,     //read data from slave
- 	output	RRESP,		//read response signal
-	output	RLAST,		//read data last signal
-	output	RVALID,		//read data valid signal
-	input	RREADY		//read ready signal
+	output logic RID,		//read data id
+	output logic RDATA,     //read data from slave
+ 	output logic RRESP,		//read response signal
+	output logic RLAST,		//read data last signal
+	output logic RVALID,		//read data valid signal
+	input logic RREADY		//read ready signal
 );
